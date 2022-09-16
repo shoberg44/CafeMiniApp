69fa6a5cf0b9799074c59d2eca6d21da5636ba4c
shoberg44 <steven.hoberg23@d155.org>
2022-09-16 17:59:52 -0500

    at home work

--- CafeMiniApp/ViewController.swift
+++ CafeMiniApp/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var priceOutput: UILabel!
     @IBOutlet weak var cartOutput: UILabel!
     @IBOutlet weak var menuLabel: UILabel!
+    @IBOutlet weak var alert: UILabel!
     
     var items = [String]()
     var prices = [Double]()
@@ -28,33 +29,109 @@ class ViewController: UIViewController {
         } //... inclusive of 1 ..< exlusisive of one
         
     */
-        
        
+        items.append("apple")
+        prices.append(2.08)
+        
+        
+        
+        
+        ///Stolen Code \/\/
+        //Looks for single or multiple taps.
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+
+        //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
+        //tap.cancelsTouchesInView = false
+
+            view.addGestureRecognizer(tap)
     }
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    var devmode = 0
     @IBAction func cartAdd(_ sender: UIButton) {
-        
-        
+        /*
+         else if textInput.text == "exit" && devmode == 1{
+             view.backgroundColor = UIColor.systemBackground
+             cartOutput.text = ""
+             textInput.text = ""
+             devmode = 0
+             break
+         }
+        */
+        //var storage : (String)
+        alert.isHidden = true
+        alert.text = "Not on the menu!"
         if textInput.text != "" {//ensure text in feild
-            for i in 0 ..< items.count{//iterate through menu array
-                if textInput.text == items[i]{//if match
-                    itemCart.append(textInput.text!)//add to your car array
-                    price += prices[i]//calculate price
-                    priceOutput.text = "$\(price)"//update price
-                    break//break loop since item found
+            print("devmode --> \(devmode)")
+            
+            
+            /*dev mode explantion
+             there is probably a better way to do it
+             
+             dev = 0 ; not in mode
+             dev = 1 ; pending init command
+             dev = 2 ; pending price
+             back to 1
+            */
+            
+            if ((textInput.text! != "exit")&&(textInput.text! != "pass")) && devmode == 1{//checks for first command ignores exit (exit is read when )
+                print(textInput.text!)
+                items.append(textInput.text!)
+                print(items)
+                textInput.text = ""
+                textInput.placeholder = "Now set a price!"
+                devmode = 2
+            }
+            else if devmode == 2{
+                if let num = Double(textInput.text!)  {
+                    prices.append(num)
+                    textInput.text = ""
+                    textInput.placeholder = "Add Item's Here"
+                    print(prices)
+                    devmode = 1
                 }
-                else{
-                    cartOutput.text = "Item doesn't exist!"
+                else {
+                    alert.text = "Invalid Input"
+                    alert.isHidden = false
                 }
-                
+            }
+            else if (textInput.text == "exit") && (devmode == 1){
+                view.backgroundColor = UIColor.systemBackground
+                cartOutput.text = ""
+                textInput.text = ""
+                devmode = 0
+            }
+            else{
+                //\//////////end of dev mode code/////
+                for i in 0 ..< items.count{//iterate through menu array
+                    if textInput.text == items[i]{//if match
+                        itemCart.append(textInput.text!)//add to your car array
+                        price += prices[i]//calculate price
+                        priceOutput.text = "$\(price)"//update price
+                        break//break loop since item found
+                        
+                    }
+                    
+                   //devmode path
+                    else if textInput.text == "pass" {
+                        view.backgroundColor = UIColor.systemRed
+                        cartOutput.text = "Manager Mode\nType 'exit' to exit mode"
+                        devmode = 1
+                        textInput.text = ""
+                        break
+                    }
+                    else{
+                        alert.isHidden = false
+                    }
+                }
             }
         }
-        
-        
-        
-        
-        //... inclusive of 1 ..< exlusisive of one
+        else{
+            textInput.placeholder = "Type in something"
+        //note... inclusive of 1 ..< exlusisive of one
+            print("blank click")
+        }
     }
-    
-
 }
-
